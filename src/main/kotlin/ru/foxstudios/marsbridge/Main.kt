c25dad628c120e7ac36baff6aa6ab9a5ed54b2de
AgniaEndie <Endienasg3@gmail.com>
2024-03-08 02:30:07 +0500

    test waiting doWork

--- src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
@@ -9,10 +9,7 @@ import reactor.netty.udp.UdpClient
 
 fun main(args: Array<String>) {
     val client: Connection =
-        UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).doOnDisconnected {
-            it.rebind(reconnect())
-            println("DISCONNECTED!!!!!!!!!!")
-        }.connectNow()
+        UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
 
     val factory = ConnectionFactory()
     factory.host = "mars-queue-service"
@@ -22,45 +19,45 @@ fun main(args: Array<String>) {
 
     val deliverCallback: DeliverCallback = DeliverCallback { _, delivery ->
         val message = String(delivery.body, charset("UTF-8"))
-        val weight = runBlocking {
-            countMessageWeight(message)
+        try{
+            doWork(client,message)
+        }finally {
+            println(" [x] Done - ok?!")
         }
-
-        println(" [x] Received '$message' weight: $weight")
-        client.outbound().sendString(Mono.just(message)).then().subscribe()
-        client.inbound().receive().asString().doOnTerminate {
-            println(
-                "disconnect! ${client.isDisposed}, ${client.channel().isOpen}, ${client.channel().isActive}, ${
-                    client.channel().remoteAddress()
-                }"
-            )
-        }
-            .doOnNext { text ->
-                println(text)
-                if (text == "ok") {
-                    channel.basicAck(delivery.envelope.deliveryTag, false)
-                    println(" [x] Done! Remove $message from queue!")
-                }
-            }
-            .doOnError { err -> println(err.message); }
-            .subscribe()
-
 
     }
 
-    channel.basicConsume("mars-queue", false, deliverCallback, { consumerTag -> })
+    channel.basicConsume("mars-queue", true, deliverCallback, { consumerTag -> })
     println("starting1")
 
     client.onDispose().block()
 }
 
-fun reconnect(): Connection {
-    Thread.sleep(100000)
-    print("reconecting......")
-    return UdpClient.create().port(25577).host("host.docker.internal").doOnDisconnected {
-        reconnect()
-    }.connectNow()
+fun doWork(client: Connection, message: String) {
+    val weight = runBlocking {
+        countMessageWeight(message)
+    }
+    println(" [x] Received '$message' weight: $weight")
+    client.outbound().sendString(Mono.just(message)).then().subscribe()
+
+    client.inbound().receive().asString().doOnTerminate {
+        println(
+            "disconnect! ${client.isDisposed}, ${client.channel().isOpen}, ${client.channel().isActive}, ${
+                client.channel().remoteAddress()
+            }"
+        )
+    }
+        .doOnNext { text ->
+            println(text)
+            if (text == "ok") {
+                //channel.basicAck(delivery.envelope.deliveryTag, false)
+                println(" [x] Done! Remove $message from queue!")
+            }
+        }
+        .doOnError { err -> println(err.message); }
+        .subscribe()
 }
+
 
 fun countMessageWeight(message: String): Int {
     return message.toByteArray(Charsets.UTF_8).size
