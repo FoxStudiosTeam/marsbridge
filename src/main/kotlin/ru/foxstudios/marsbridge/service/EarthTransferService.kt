4b1e619eee3d942903c92ac48a8b9c8087d1740f
AgniaEndie <Endienasg3@gmail.com>
2024-03-08 04:00:52 +0500

    add logger

--- src/main/kotlin/ru/foxstudios/marsbridge/service/EarthTransferService.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/service/EarthTransferService.kt
@@ -3,15 +3,18 @@ package ru.foxstudios.marsbridge.service
 import com.rabbitmq.client.ConnectionFactory
 import com.rabbitmq.client.DeliverCallback
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
 import reactor.netty.Connection
 import reactor.netty.udp.UdpClient
 
 class EarthTransferService() {
     var client: Connection? = null
+    val logger = LoggerFactory.getLogger(this::class.java)
 
     init {
         val factory = ConnectionFactory()
+
         factory.host = "mars-queue-service"
         factory.port = 5672
         val connection: com.rabbitmq.client.Connection = factory.newConnection()
@@ -21,7 +24,7 @@ class EarthTransferService() {
             try {
                 doWork(message)
             } finally {
-                println(" [x] Done - ok?!")
+                logger.info(" [x] Done - ok?!")
             }
 
         }
@@ -32,16 +35,16 @@ class EarthTransferService() {
     fun doWork(message: String) {
 
         client = UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
-        println(" [d] isDisposed true ${client!!.isDisposed}")
+        logger.info(" [d] isDisposed true ${client!!.isDisposed}")
 
         val weight = runBlocking {
             countMessageWeight(message)
         }
-        println(" [x] Received '$message' weight: $weight")
+        logger.info(" [x] Received '$message' weight: $weight")
         client!!.outbound().sendString(Mono.just(message)).then().subscribe()
 
         client!!.inbound().receive().asString().doOnTerminate {
-            println(
+            logger.info(
                 "disconnect! ${client!!.isDisposed}, ${client!!.channel().isOpen}, ${client!!.channel().isActive}, ${
                     client!!.channel().remoteAddress()
                 }"
@@ -50,10 +53,10 @@ class EarthTransferService() {
             .doOnNext { text ->
                 println(text)
                 if (text == "ok") {
-                    println(" [x] Done! Remove $message from queue!")
+                    logger.info(" [x] Done! Remove $message from queue!")
                 }
             }
-            .doOnError { err -> println(err.message); }
+            .doOnError { err -> logger.info(err.message); }
             .subscribe()
     }
 
