752c5b4d58a88e3aa923d3d8ba7428240c3f462e
AgniaEndie <Endienasg3@gmail.com>
2024-03-08 03:21:15 +0500

    test EarthTransferService.kt

--- src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
@@ -1,68 +1,9 @@
 package ru.foxstudios.marsbridge
 
-import com.rabbitmq.client.ConnectionFactory
-import com.rabbitmq.client.DeliverCallback
-import kotlinx.coroutines.runBlocking
-import reactor.core.publisher.Mono
-import reactor.netty.Connection
-import reactor.netty.udp.UdpClient
+import ru.foxstudios.marsbridge.service.EarthTransferService
 
 fun main(args: Array<String>) {
-    var client: Connection =
-        UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
-
-    val factory = ConnectionFactory()
-    factory.host = "mars-queue-service"
-    factory.port = 5672
-    val connection: com.rabbitmq.client.Connection = factory.newConnection()
-    val channel = connection.createChannel()
-
-    val deliverCallback: DeliverCallback = DeliverCallback { _, delivery ->
-        val message = String(delivery.body, charset("UTF-8"))
-        try{
-            doWork(client,message)
-        }finally {
-            println(" [x] Done - ok?!")
-        }
-
-    }
-
-    channel.basicConsume("mars-queue", true, deliverCallback, { consumerTag -> })
-    println("starting1")
-
-    client.onDispose().block()
-}
-
-fun doWork(client: Connection, message: String) {
-    var localClient = client
-    if(client.isDisposed){
-        localClient = UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
-        println(" [d] isDisposed true ${localClient.isDisposed}")
-    }
-    val weight = runBlocking {
-        countMessageWeight(message)
-    }
-    println(" [x] Received '$message' weight: $weight")
-    localClient.outbound().sendString(Mono.just(message)).then().subscribe()
-
-    localClient.inbound().receive().asString().doOnTerminate {
-        println(
-            "disconnect! ${localClient.isDisposed}, ${localClient.channel().isOpen}, ${localClient.channel().isActive}, ${
-                client.channel().remoteAddress()
-            }"
-        )
-    }
-        .doOnNext { text ->
-            println(text)
-            if (text == "ok") {
-                println(" [x] Done! Remove $message from queue!")
-            }
-        }
-        .doOnError { err -> println(err.message); }
-        .subscribe()
+    var service = EarthTransferService()
 }
 
 
-fun countMessageWeight(message: String): Int {
-    return message.toByteArray(Charsets.UTF_8).size
-}
---
+++ src/main/kotlin/ru/foxstudios/marsbridge/service/EarthTransferService.kt
@@ -0,0 +1,66 @@
+package ru.foxstudios.marsbridge.service
+
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
+import kotlinx.coroutines.runBlocking
+import reactor.core.publisher.Mono
+import reactor.netty.Connection
+import reactor.netty.udp.UdpClient
+
+class EarthTransferService() {
+    var client: Connection? = null
+
+    init {
+        client = UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
+        val factory = ConnectionFactory()
+        factory.host = "mars-queue-service"
+        factory.port = 5672
+        val connection: com.rabbitmq.client.Connection = factory.newConnection()
+        val channel = connection.createChannel()
+        val deliverCallback: DeliverCallback = DeliverCallback { _, delivery ->
+            val message = String(delivery.body, charset("UTF-8"))
+            try {
+                doWork(message)
+            } finally {
+                println(" [x] Done - ok?!")
+            }
+
+        }
+        channel.basicConsume("mars-queue", true, deliverCallback, { consumerTag -> })
+        println("starting1")
+        client!!.onDispose().block()
+    }
+
+    fun doWork(message: String) {
+        if (client!!.isDisposed) {
+            client = UdpClient.create().port(25577).host("host.docker.internal").wiretap(true).connectNow()
+            println(" [d] isDisposed true ${client!!.isDisposed}")
+        }
+        val weight = runBlocking {
+            countMessageWeight(message)
+        }
+        println(" [x] Received '$message' weight: $weight")
+        client!!.outbound().sendString(Mono.just(message)).then().subscribe()
+
+        client!!.inbound().receive().asString().doOnTerminate {
+            println(
+                "disconnect! ${client!!.isDisposed}, ${client!!.channel().isOpen}, ${client!!.channel().isActive}, ${
+                    client!!.channel().remoteAddress()
+                }"
+            )
+        }
+            .doOnNext { text ->
+                println(text)
+                if (text == "ok") {
+                    println(" [x] Done! Remove $message from queue!")
+                }
+            }
+            .doOnError { err -> println(err.message); }
+            .subscribe()
+    }
+
+
+    fun countMessageWeight(message: String): Int {
+        return message.toByteArray(Charsets.UTF_8).size
+    }
+}
