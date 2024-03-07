78ed7659b2b23641762f023f3237b5af5ec39452
AgniaEndie <Endienasg3@gmail.com>
2024-03-08 01:05:33 +0500

    reuse logging

--- src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
@@ -30,7 +30,7 @@ fun main(args: Array<String>) {
             println(" [x] Received '$message' weight: $weight")
             client.outbound().sendString(Mono.just(message)).then().subscribe()
             client.inbound().receive().asString().doOnTerminate {
-                println("disconnect! ${client.isDisposed}")
+                println("disconnect! ${client.isDisposed}, ${client.channel().isOpen}, ${client.channel().isActive}, ${client.channel().remoteAddress()}")
             }
                 .doOnNext { text ->
                     println(text)
