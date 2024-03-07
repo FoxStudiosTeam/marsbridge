bba0723327fc7725f08e77ebd912fcd92c7e7bec
AgniaEndie <Endienasg3@gmail.com>
2024-03-07 22:31:57 +0500

    update reconnection

--- src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/Main.kt
@@ -1,5 +1,6 @@
 package ru.foxstudios.marsbridge
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import reactor.netty.Connection
@@ -7,12 +8,17 @@ import reactor.netty.udp.UdpClient
 import ru.foxstudios.marsbridge.service.EarthTransferService
 
 fun main(args: Array<String>) {
-    val client: Connection = UdpClient.create().port(25577).host("host.docker.internal").connectNow()
+    val client: Connection = UdpClient.create().port(25577).host("host.docker.internal").doOnDisconnected {
+        it.rebind(reconnect())
+    }
+        .connectNow()
     val rmqService = EarthTransferService(client)
     runBlocking {
         launch {
+            client.inbound().receive().asString().doOnTerminate {
 
-            client.inbound().receive().asString().doOnTerminate { println("terminated") }
+
+            }
                 .doOnNext { text -> println(text) }
                 .doOnError { err -> println(err.message); client.disposeNow() }
                 .subscribe()
@@ -22,4 +28,12 @@ fun main(args: Array<String>) {
         println("starting1")
     }
     client.onDispose().block()
+}
+
+fun reconnect(): Connection {
+    Thread.sleep(100000)
+    print("reconecting......")
+    return UdpClient.create().port(25577).host("host.docker.internal").doOnDisconnected {
+        reconnect()
+    }.connectNow()
 }
