1d3b072f5ee438772af0af2d1d11563f3757b04f
AgniaEndie <Endienasg3@gmail.com>
2024-03-10 12:57:44 +0500

    logging

--- src/main/kotlin/ru/foxstudios/marsbridge/service/EarthTransferService.kt
+++ src/main/kotlin/ru/foxstudios/marsbridge/service/EarthTransferService.kt
@@ -41,6 +41,7 @@ class EarthTransferService {
 
     fun doWork(message: String, channel: Channel, delivery: Delivery) {
         //"host.docker.internal"
+        println("test-earth-ip ${System.getenv("EARTH_IP")}")
         client = UdpClient.create().port(30015).host(System.getenv("EARTH_IP")).wiretap(true).option(ChannelOption.SO_SNDBUF,
             8192).connectNow()
         println(" [d] isDisposed true ${client!!.isDisposed}")
